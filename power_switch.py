1a93066f2863bbeabc2e799c3fd8343db822d54d
Tanish <tanishbhavsar2007@gmail.com>
2026-01-10 12:27:28 +0530

    Add device clear functionality before shutdown in power switch monitoring

--- power_switch.py
+++ power_switch.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import os
+from display import device   # 🔥 IMPORTANT
 
 POWER_PIN = 27  # GPIO27
 
@@ -9,8 +10,12 @@ GPIO.setup(POWER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def monitor_power_switch():
     while True:
-        if GPIO.input(POWER_PIN):  # switch OFF
-            time.sleep(0.3)        # debounce
+        if GPIO.input(POWER_PIN):   # switch OFF
+            time.sleep(0.3)         # debounce
             if GPIO.input(POWER_PIN):
+                try:
+                    device.clear()  # 🔥 clear OLED before shutdown
+                except:
+                    pass
                 os.system("sudo shutdown now")
         time.sleep(0.1)
